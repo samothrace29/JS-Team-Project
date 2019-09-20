f044e4e34be0e2bbbc2f3dcea58a6aeae209bc36
samothrace29 <34971701+samothrace29@users.noreply.github.com>
2019-09-20 14:44:36 +0200

    good v

--- script/script.js
+++ script/script.js
@@ -30,6 +30,7 @@ const body = document.querySelector("body");
 //console.log(body);
 
 function clearAll(){
+    const cube = document.querySelector("#target");
     cube.removeEventListener("click", touched);
     document.removeEventListener("keydown",moving);
     clearTimeout ( counterTimeOut );
@@ -37,7 +38,7 @@ function clearAll(){
 }
 
 function touched(e) {
-    //const cube = document.querySelector("#target");
+    const cube = document.querySelector("#target");
     //cube.removeEventListener("click", touched);
     //clearTimeout ( counterTimeOut );
     //document.removeEventListener("keydown",moving);
@@ -52,6 +53,7 @@ function touched(e) {
 function timerDuckGame() {
     cube.style.backgroundColor = "green";
     alert ("victory of the player at the keyboard");
+    clearAll();
     victory(playerPlusScore[0].querySelector("h1").textContent);
 }
 
