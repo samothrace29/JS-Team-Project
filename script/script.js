e5b8b3c7f3e195de9c6a6a97f673f0018e0efe85
samothrace29 <34971701+samothrace29@users.noreply.github.com>
2019-09-20 10:04:36 +0200

    mouse crosshair

--- script/script.js
+++ script/script.js
@@ -1,9 +1,31 @@
 
+// Create the click event on cube
 const cube = document.querySelector("#target");
-
 cube.addEventListener("click", touched);
 
-function touched() {
-alert("The game is over: it's victory!");
-//removeEventListener("click", this);
+// timeout is execute only one time, using setInterval for a recursive call
+setTimeout(timerDuckGame, 5000);
+
+// 
+
+const body = document.querySelector("body");
+
+console.log(body);
+body.style.cursor = "crosshair";
+
+
+
+function touched(e) {
+    alert("You win!");
+    const cube = document.querySelector("#target");
+    cube.removeEventListener("click", touched);
+    
+    cube.style.backgroundColor = "red";
 };
+
+
+function timerDuckGame() {
+    cube.style.backgroundColor = "yellow";
+    cube.removeEventListener("click", touched);
+    alert ("victory of the player at the keyboard");
+}
