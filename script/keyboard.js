c469a301f4b474c2c22fed32642feb1613efa58e
daudy85 <55229575+daudy85@users.noreply.github.com>
2019-09-20 10:35:07 +0200

    top right problem

--- script/keyboard.js
+++ script/keyboard.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("keydown",moving);
 let topValue=40;
-
+let leftValue=40;
 function moving(event){
 
 
@@ -11,10 +11,37 @@ function moving(event){
     topValue +=5;     
  }
  if (topValue == 90){
-     topValue = 0;
+     topValue = 0 ;
  
     }
-console.log()
+if (event.code==="ArrowLeft"){
+    leftValue -=5 ;
+
+}
+if(leftValue == 0){
+    leftValue = 90;
+
+}
+ if (event.code==="ArrowUp"){
+    topValue -=5; 
+     if (topValue == 0){
+     topValue = 90 ;   
+ }
+ 
+ 
+    }
+if (event.code==="ArrowRight"){
+    leftValue +=5 ;
+
+}
+if(leftValue == 90){
+    leftValue = 10;
+
+}
+
+console.log(topValue,leftValue)
 document.querySelector("#target").style.top = topValue + "%";
+document.querySelector("#target").style.left = leftValue + "%";
+
 
 }
